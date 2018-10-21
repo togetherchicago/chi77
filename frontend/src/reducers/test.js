28f5c2f85b9c59be78a7b7d64ab0547618387853
Ben Heath <benheath@umich.edu>
2018-10-21 13:04:42 -0400

    added reducers for redux

---
+++ frontend/src/reducers/test.js
@@ -0,0 +1,13 @@
+const test = (state = [], action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+          return state + 1
+        case 'DECREMENT':
+          return state - 1
+        default:
+          return state
+      }
+  }
+
+  export default test
+  
