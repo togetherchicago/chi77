4031b9cbb6178019090c0ecf4d67585b094966cf
Ben Heath <benheath@umich.edu>
2018-10-21 13:04:51 -0400

    added redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers'
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(rootReducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+ document.getElementById('root')
+);
 registerServiceWorker();
