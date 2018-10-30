c206178f7357a922c1be0791acd010f7e86fc6fa
Mo Zebdi <mozebdi@no-reply.users.github.com>
2018-10-30 19:40:46 -0400

    generate_equivalencies works now, I think

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -6,8 +6,8 @@ echo -e "Done migrating!"
 echo -e "Loading data..."
 echo -e "from chicagomap.load import run\nrun()" | python3 manage.py shell
 echo -e "Done loading data!"
-# echo -e "Generating equivalencies..."
-# python3 manage.py generate_equivalencies
-# echo -e "Done generating equivalencies!"
+echo -e "Generating equivalencies..."
+python3 manage.py generate_equivalencies
+echo -e "Done generating equivalencies!"
 # python3 manage.py populate_db
 
