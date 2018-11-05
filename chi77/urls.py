f2cda3a47cd66c11028c5ab128feba572385b62e
Karan Kwatra <kwatraka@umich.edu>
2018-11-05 14:49:03 -0500

    Throttling & DB Changes (#18)
    * rewriting domain transformation logic
    
    * reconciled foreign key relationship in serializer
    
    * Population now works with fixed backend issue
    
    * Added Lodash and debounce function to the slider filter UI
    
    * removed Cypress
    
    * changed styling from hardcoded for population to more fluid

--- chi77/urls.py
+++ chi77/urls.py
@@ -28,19 +28,19 @@ urlpatterns = [
 
     path('api/<str:dataset>/', api.dataset_list),
     path('api/<str:dataset>/<str:domain>/', api.dataset_list_domain),
-    path('api/<str:dataset>/<slug:date>', api.dataset_list_date),
-    path('api/<str:dataset>/<slug:date>/<str:domain>/', api.dataset_list_date_domain),
+    # path('api/<str:dataset>/<slug:date>', api.dataset_list_date),
+    # path('api/<str:dataset>/<slug:date>/<str:domain>/', api.dataset_list_date_domain),
 
-    path('api/population/', api.population_list),
-    path('api/population/wards/', api.population_list_wards),
-    path('api/population/wards/<str:domain>', api.population_list_domain),
+    # path('api/population/', api.population_list),
+    # path('api/population/wards/', api.population_list_wards),
+    # path('api/population/wards/<str:domain>', api.population_list_domain),
 
-    path('api/domain/', api.domain_list),
-    path('api/domain/tracts/', api.tract_list),
-    path('api/domain/zips/', api.zip_list),
-    path('api/domain/neighborhoods/', api.neighborhood_list),
-    path('api/domain/precincts/', api.precinct_list),
-    path('api/domain/wards/', api.ward_list),
+    # path('api/domain/', api.domain_list),
+    # path('api/domain/tracts/', api.tract_list),
+    # path('api/domain/zips/', api.zip_list),
+    # path('api/domain/neighborhoods/', api.neighborhood_list),
+    # path('api/domain/precincts/', api.precinct_list),
+    # path('api/domain/wards/', api.ward_list),
 
     path('', TemplateView.as_view(template_name='index.html')),
 
