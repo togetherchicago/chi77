905df99d759c19b6a653ba6887548375a29faf04
ReactiveXYZ-Dev <jackie@reactive.xyz>
2019-03-24 17:29:43 -0400

    Minor URL change

--- getdata/urls.py
+++ getdata/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
 
-    path('general/', view_general_data),
+    path('general/view/', view_general_data),
 
     path('transit/view/', view_transit_data),
     path('healthcare/view/', view_healthcare_data),
