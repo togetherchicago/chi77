34fef3ff94259ab7bda93c0b3e24f23bd2f9148c
licurtis <licurtis@umich.edu>
2019-10-09 21:41:15 -0400

    minor App changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,42 +19,43 @@ class App extends Component {
 
   render() {
     return (
-      <Container>
-        <Navbar style={{ "padding": "15px" }} bg="dark" variant="dark">
-          <Navbar.Brand href="/">
-            <img
-              alt=""
-              src={ logo }
-              width="80"
-              height="30"
-              className="d-inline-block align-top"
-            />
-          </Navbar.Brand>
-          <Button style={{ "margin-left": "1000px" }} variant="outline-light">Reset Filters</Button>
-        </Navbar>
+      <Container style={{"height": "100vh"}} fluid={true}>
+        <Row>
+          <Navbar style={{"width": "100%"}} bg="dark" variant="dark">
+            <Navbar.Brand href="/">
+              <img
+                alt=""
+                src={ logo }
+                width="80"
+                height="30"
+                className="d-inline-block align-top"
+              />
+            </Navbar.Brand>
+            <Button style={{"marginLeft": "auto"}} variant="outline-light">Reset Filters</Button>
+          </Navbar>
+        </Row>
 
-        <Container style={{ "height": "100%", "padding-left": "0", "padding-right": "0" }}>
-          <Row style={{"height": "100%", "padding-left": "0", "padding-right": "0" }}>
-            <Col md={{ span: 3 }}>
-              <React.Fragment style={{ "background-color": "lightgray" }}>
-                <DropdownButton style={{"padding": "20px", "align-content": "left"}}variant="secondary" id="dropdown-secondary-button" title="TRANSPORTATION">
-                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                  <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                </DropdownButton>
+        <Row>
+          <Col md={{ span: 3 }}>
+            <React.Fragment>
+              <DropdownButton style={{"padding": "20px", "alignContent": "left"}} variant="secondary" id="dropdown-secondary-button" title="TRANSPORTATION">
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </DropdownButton>
 
-                <DropdownButton style={{"width": "50px", "padding": "20px", "align-content": "left"}}variant="secondary" id="dropdown-secondary-button" title="HEALTHCARE">
-                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                  <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                </DropdownButton>
+              <DropdownButton style={{"width": "50px", "padding": "20px", "alignContent": "left"}} variant="secondary" id="dropdown-secondary-button" title="HEALTHCARE">
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </DropdownButton>
+            </React.Fragment>
+          </Col>
 
-              </React.Fragment>
-            </Col>
-
-            <Col md={{ span: 9 }}> <MapConn style={{"padding-left": "0", "padding-right": "0"}}/></Col>
-          </Row>
-        </Container>
+          <Col md={{ span: 9 }}>
+            <MapConn />
+          </Col>
+        </Row>
       </Container>
     );
   }
