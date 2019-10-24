e4d6fc592d86c70c9746500b1a6e9ecf6b0045af
licurtis <licurtis@umich.edu>
2019-10-24 16:37:31 -0400

    merge fetch and parse functions in effects

--- frontend/src/chicago-health-atlas/effects.js
+++ frontend/src/chicago-health-atlas/effects.js
@@ -1,12 +1,22 @@
-import { put, call, select, take } from 'redux-saga/effects';
+import { put, select, take } from 'redux-saga/effects';
 
 import { api } from '../api';
 import { addPlacesAC, addHospitalsAC, addTrainStationsAC } from './actions';
 import { getCommunityAreas } from '../selectors';
 
-function parsePlaces(data) {
+export function * fetchPlaces() {
+  const response = yield api.get('/general/view/', {
+    params: {
+      resource: 'chi_health_atlas',
+      category: 'places',
+      query: JSON.stringify({}),
+    },
+  });
+
+  const data = response.data.data;
   const relevantKeys = ['part', 'resource_cnt', 'name'];
   const communityAreas = {};
+
   for (const place of data['community_areas']) {
 
     const info = {};
@@ -24,38 +34,6 @@ function parsePlaces(data) {
 
     communityAreas[place['slug']] = info;
   }
-  return communityAreas;
-}
-
-function parseHospitals(data) {
-  const hospitals = {};
-
-  for (const h of data) {
-    // Grab slug
-    const slug = h['slug'];
-
-    // Parse latLong
-    const latLong = h['lat_long'].split(',');
-    latLong[0] = parseFloat(latLong[0]);
-    latLong[1] = parseFloat(latLong[1]);
-    h['lat_long'] = latLong;
-
-    hospitals[slug] = h;
-  }
-
-  return hospitals;
-}
-
-export function * fetchPlaces() {
-  const response = yield api.get('/general/view/', {
-    params: {
-      resource: 'chi_health_atlas',
-      category: 'places',
-      query: JSON.stringify({}),
-    },
-  });
-
-  const communityAreas = yield call(parsePlaces, response.data.data);
 
   yield put(addPlacesAC({ communityAreas }));
 }
@@ -74,7 +52,21 @@ export function * fetchHospitals() {
     });
 
     if (response.data.data) {
-      const hospitals = yield call(parseHospitals, response.data.data);
+      const data = response.data.data;
+      const hospitals = {};
+
+      for (const h of data) {
+        // Grab slug
+        const slug = h['slug'];
+
+        // Parse latLong
+        const latLong = h['lat_long'].split(',');
+        latLong[0] = parseFloat(latLong[0]);
+        latLong[1] = parseFloat(latLong[1]);
+        h['lat_long'] = latLong;
+
+        hospitals[slug] = h;
+      }
 
       yield put(addHospitalsAC({ hospitals, area }));
     }
